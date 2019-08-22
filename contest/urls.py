9c893194b7d4e990b1313fe45c1781b297ff5927
pouria <pooriazmn@gmail.com>
2019-08-22 17:51:28 +0430

    only scoreboard will served throgh public api

--- contest/urls.py
+++ contest/urls.py
@@ -3,6 +3,4 @@ from contest import views
 
 urlpatterns = [
     path('scoreboard/', views.ScoreboardView.as_view()),
-    path('duels/', views.DuelRequestView.as_view()),
-    path('get-problem', views.SolveAttemptView.as_view()),
 ]
