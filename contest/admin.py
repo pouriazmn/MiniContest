f8ca3423b2bca143524a5bf673f9be2cbd1794c8
pouria <pooriazmn@gmail.com>
2019-08-22 17:33:48 +0430

    commented out old admin soloution

--- contest/admin.py
+++ contest/admin.py
@@ -3,26 +3,26 @@ from django import forms
 from .models import *
 
 
-@admin.register(Team)
-class TeamAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'strength', 'pending_duels_display', 'active_problems_display')
+# @admin.register(Team)
+# class TeamAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'name', 'strength', 'pending_duels_display', 'active_problems_display')
 
 
-# class SolvingAttemptForm(forms.ModelForm):
-#     purchase_from = forms.ModelChoiceField(queryset=SolvingAttempt.objects.filter(problem=))
-#
+# # class SolvingAttemptForm(forms.ModelForm):
+# #     purchase_from = forms.ModelChoiceField(queryset=SolvingAttempt.objects.filter(problem=))
+# #
 
-@admin.register(SolvingAttempt)
-class SolvingAttemptAdmin(admin.ModelAdmin):
-    readonly_fields = ('is_purchased', 'get_duration')
-    list_display = ('id', 'team_id', 'problem', 'state', 'grade', 'get_duration',
-                    'purchased_from', 'purchased_timedelta', 'purchased_grade', 'purchase_cost')
-    list_filter = ('team', 'problem', 'state')
+# @admin.register(SolvingAttempt)
+# class SolvingAttemptAdmin(admin.ModelAdmin):
+#     readonly_fields = ('is_purchased', 'get_duration')
+#     list_display = ('id', 'team_id', 'problem', 'state', 'grade', 'get_duration',
+#                     'purchased_from', 'purchased_timedelta', 'purchased_grade', 'purchase_cost')
+#     list_filter = ('team', 'problem', 'state')
 
 
-@admin.register(Duel)
-class DuelAdmin(admin.ModelAdmin):
-    list_display = ('id', 'requested_by', 'to', 'problem', 'pending', 'worth', 'winner')
+# @admin.register(Duel)
+# class DuelAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'requested_by', 'to', 'problem', 'pending', 'worth', 'winner')
 
 
 admin.site.register(Problem)
