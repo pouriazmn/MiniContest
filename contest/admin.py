cf602238269ec3133c2ab0bfb6b8a046df976b77
pouria <pooriazmn@gmail.com>
2019-08-22 21:10:45 +0430

    request-problem admin action

--- contest/admin.py
+++ contest/admin.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.http import HttpResponseRedirect
 from django.template.response import TemplateResponse
 from django.urls import re_path, reverse
 from django.utils.html import format_html
@@ -24,17 +25,31 @@ class TeamAdmin(admin.ModelAdmin):
                 self.admin_site.admin_view(self.process_solve_attempt),
                 name='solve-attempt',
             ),
+            re_path(
+                r'^(?P<team_id>.+)/return-problem/$',
+                self.admin_site.admin_view(self.process_return_problem),
+                name='return-problem',
+            ),
         ]
         return custom_urls + urls
 
     def team_actions(self, obj):
         return format_html(
+            '<a class="button" href="{}">request problem</a>&nbsp;'
             '<a class="button" href="{}">request problem</a>&nbsp;',
             reverse('admin:solve-attempt', args=[obj.pk]),
+            reverse('admin:return-problem', args=[obj.pk])
         )
 
     team_actions.short_description = 'Team Actions'
     team_actions.allow_tags = True
+
+    def proccess_return_problem(self, request, team_id, *args, **kwargs):
+        self.procces_actioni(
+            request=request,
+            team_id=team_id,
+            action_form=
+        )
 
     def process_solve_attempt(self, request, team_id, *args, **kwargs):
         return self.process_action(
@@ -52,24 +67,22 @@ class TeamAdmin(admin.ModelAdmin):
         team = self.get_object(request, team_id)
 
         if request.method == 'POST':
-            form = action_form(request.POST)
+            form = action_form(request.POST, team_id=team_id)
             if form.is_valid():
                 try:
-                    form.save(account, request.user)
-                except errors.Error as e:
-                    # If save() raised, the form will a have a non
-                    # field error containing an informative message.
-                    pass
+                    form.save()
+                except Exception as e:
+                    self.message_user(request, f'sth went wrong: {str(e)}', level=messages.ERROR)
                 else:
                     self.message_user(request, 'Success')
                     url = reverse(
-                        'admin:contest_team_change',
-                        args=[team.pk],
+                        'admin:contest_team_changelist',
+                        # args=[team_id],
                         current_app=self.admin_site.name,
                     )
                     return HttpResponseRedirect(url)
 
-        form = action_form()
+        form = action_form(team_id=team_id)
         context = self.admin_site.each_context(request)
         context['opts'] = self.model._meta
         context['form'] = form
