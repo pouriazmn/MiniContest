53a30874db167421aaebaa644dee500600f098d6
pouria <pooriazmn@gmail.com>
2019-08-28 21:21:33 +0430

    renamed project to MiniContest

--- MiniContest/wsgi.py
+++ MiniContest/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for SummerContest1398 project.
+WSGI config for MiniContest project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SummerContest1398.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MiniContest.settings')
 
 application = get_wsgi_application()
